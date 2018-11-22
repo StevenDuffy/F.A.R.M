72e7ae98a39eab708223aaea9d04d7250a261279
Kyle-Hawthorne <kylehawthorne@outlook.com>
2018-11-22 22:22:02 +0000

    Merge branch 'TheoWPF'

--- Just Ripe Farm/F.A.R.M/Properties/Settings.Designer.cs
+++ Just Ripe Farm/F.A.R.M/Properties/Settings.Designer.cs
@@ -26,8 +26,6 @@ namespace F.A.R.M.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\FarmDB.mdf;In" +
-            "tegrated Security=True;Connect Timeout=30")]
         public string ConnectFarmDB {
             get {
                 return ((string)(this["ConnectFarmDB"]));
