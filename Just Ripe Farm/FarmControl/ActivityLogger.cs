2cb721b979695a27762b3a4c5247be6c000ea24b
Taxford24 <theo.anglia@gmail.com>
2018-12-07 18:56:52 +0000

    activityLogger class created

---
+++ Just Ripe Farm/FarmControl/ActivityLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FarmControl
+{
+    public class ActivityLogger
+    {
+        private StreamWriter activityRecorder;
+        
+            private static ActivityLogger logger;
+
+        public static ActivityLogger Logger
+        {
+            get
+            {
+                if (logger == null)
+                {
+                    logger = new ActivityLogger();
+                }
+
+                return logger;
+            }   
+        }
+    }
+}
+
